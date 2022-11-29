98ccc0abb86a68056bda60e9228cb7aca0f185d5
Afolabi <opakunleafolabi@gmail.com>
2022-11-29 15:23:21 +0100

    base bootstrap included

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostFormComponent } from './post-form/post-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'make-post', pathMatch: 'full'},
+  { path: 'make-post', component: PostFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
