04ba6d17521215a0be7128179d9ff994f5b085d9
Afolabi <opakunleafolabi@gmail.com>
2022-11-29 18:45:28 +0100

    recipes post implemented

--- src/app/post-form/post-recipes.component.ts
+++ src/app/post-form/post-recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-post-recipes',
@@ -10,7 +11,7 @@ export class PostRecipesComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.buildForm();
@@ -27,6 +28,12 @@ export class PostRecipesComponent implements OnInit {
 
   submit() {
     console.log(this.form.value);
+    this.http.post('https://angular-update-af322-default-rtdb.firebaseio.com/recipes.json', this.form.value)
+      .subscribe({
+        next: (response) => {
+          console.log(response)
+        }
+      })
     this.form.reset()
   }
 
